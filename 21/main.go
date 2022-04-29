ee856f38f5e7973ac3e0037db08b7906e95553a3
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-04-29 16:54:17 +0400

    ts/go for #21

---
+++ 21/main.go
@@ -0,0 +1,25 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// recursive
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	}
+	list2.Next = mergeTwoLists(list1, list2.Next)
+	return list2
+
+}
+
+func merge()
