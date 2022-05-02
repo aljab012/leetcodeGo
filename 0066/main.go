696f78930c1f13e9c8389f185eb344d56fb2a627
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-05-02 16:41:07 +0400

    ts/go for #66

---
+++ 0066/main.go
@@ -0,0 +1,21 @@
+import ("fmt")
+
+func plusOne(digits []int) []int {
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
+			digits[i] = 0
+		} else {
+			digits[i] += 1
+			return digits
+		}
+	}
+	digits = append([]int{1}, digits...)
+	return digits
+
+}
+
+func main(){
+	arr := [9]
+	fmt.Println(plusOne(arr))
+}
