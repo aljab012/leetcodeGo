81a415dcfb63abb0bad6c1b51498ebac8c940724
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-06-06 23:45:09 +0400

    remove typescript solutions

--- 0121/index.ts
+++
@@ -1,9 +0,0 @@
-function maxProfit(prices: number[]): number {
-  let minPrice = Number.POSITIVE_INFINITY
-  let maxProfit = 0
-  for (let i = 0; i < prices.length; i++) {
-    minPrice = Math.min(minPrice, prices[i])
-    maxProfit = Math.max(maxProfit, prices[i] - minPrice)
-  }
-  return maxProfit
-}
