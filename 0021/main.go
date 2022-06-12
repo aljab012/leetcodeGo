2585205bd3e24dd2eac5e1e36b7e72d2f3eb3580
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-06-12 19:51:28 +0400

    update #21

--- 0021/main.go
+++ 0021/main.go
@@ -6,45 +6,30 @@ type ListNode struct {
 }
 
 // idea: base case: if one of them if null, return the other. otherwise, compare the values. merge the later and next in a recursive call.
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	if list1 == nil {
-		return list2
-	}
-	if list2 == nil {
-		return list1
-	}
-	if list1.Val < list2.Val {
-		list1.Next = mergeTwoLists(list1.Next, list2)
-		return list1
-	}
-	list2.Next = mergeTwoLists(list1, list2.Next)
-	return list2
-
-}
-
-func mergeTwoLists2(list1 *ListNode, list2 *ListNode) *ListNode {
-
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	dummy := new(ListNode)
 
 	cur := dummy
-	for ; list1 != nil && list2 != nil; cur = cur.Next {
-		if list1.Val < list2.Val {
-			cur.Next = list1
-			list1 = list1.Next
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			cur.Next = &ListNode{l1.Val, nil}
+			cur = cur.Next
+			l1 = l1.Next
 		} else {
-			cur.Next = list2
-			list2 = list2.Next
+			cur.Next = &ListNode{l2.Val, nil}
+			cur = cur.Next
+			l2 = l2.Next
 		}
 	}
-	for list1 != nil {
-		cur.Next = list1
-		list1 = list1.Next
+	for l1 != nil {
+		cur.Next = &ListNode{l1.Val, nil}
 		cur = cur.Next
+		l1 = l1.Next
 	}
-	for list2 != nil {
-		cur.Next = list2
-		list2 = list2.Next
+	for l2 != nil {
+		cur.Next = &ListNode{l2.Val, nil}
 		cur = cur.Next
+		l2 = l2.Next
 	}
 	return dummy.Next
 }
