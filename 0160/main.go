fdbb4d0a916d1ecad17546a01ad75ff1e5c1905e
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-07-03 22:11:46 +0400

    Go #160

---
+++ 0160/main.go
@@ -0,0 +1,23 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	l1, l2 := headA, headB
+	for l1 != l2 {
+		if l1 == nil {
+			l1 = headB
+		} else {
+			l1 = l1.Next
+		}
+		if l2 == nil {
+			l2 = headA
+		} else {
+			l2 = l2.Next
+		}
+	}
+	return l1
+}
