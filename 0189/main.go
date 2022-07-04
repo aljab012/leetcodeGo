58a34a529f47a98e98242345a3589be8689353ac
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-07-04 19:09:56 +0400

    Update #189
    Constant memory solution

--- 0189/main.go
+++ 0189/main.go
@@ -1,12 +1,24 @@
 package main
 
+import "fmt"
+
 func rotate(nums []int, k int) {
 	k = k % len(nums)
-	startK := len(nums) - k
-	temp := append(append([]int{}, nums[startK:]...), nums[:startK]...)
-	copy(nums, temp)
+	nums = reverse(nums, 0, len(nums)-1)
+	nums = reverse(nums, 0, k-1)
+	nums = reverse(nums, k, len(nums)-1)
+}
+
+func reverse(arr []int, l, r int) []int {
+	for l < r {
+		arr[l], arr[r] = arr[r], arr[l]
+		l++
+		r--
+	}
+	return arr
 }
 
 func main() {
+	fmt.Print()
 	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 3)
 }
