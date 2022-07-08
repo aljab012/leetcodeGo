3bfb2581c6eac6a8548aefed59d1e4c6b36e57c2
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-07-08 11:29:42 +0400

    Go #206

---
+++ 0206/main.go
@@ -0,0 +1,19 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+
+	var prev, cur *ListNode = nil, head
+
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
