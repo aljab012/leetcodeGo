05837a96098df37adfacd04feed4b64b675a4460
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-07-09 17:23:57 +0400

    Go #230

---
+++ 0230/main.go
@@ -0,0 +1,28 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+	count := 0
+	stack := []*TreeNode{}
+
+	cur := root
+	for cur != nil || len(stack) > 0 {
+		for cur != nil {
+			stack = append(stack, cur)
+			cur = cur.Left
+		}
+		cur = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		count++
+		if count == k {
+			return cur.Val
+		}
+		cur = cur.Right
+	}
+	return -1
+}
