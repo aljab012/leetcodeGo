43e84c80c3fd8cd6249dfc57c790611229dc62d6
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-07-10 21:04:43 +0400

    Go #235

---
+++ 0235/main.go
@@ -0,0 +1,17 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else {
+		return root
+	}
+}
