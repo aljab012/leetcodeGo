4a1bbb5c4418d1daecfe770a7f56a4e41fd0d13e
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-07-15 10:16:53 +0400

    add iterative to #226

--- 0226/main.go
+++ 0226/main.go
@@ -16,3 +16,26 @@ func invertTree(root *TreeNode) *TreeNode {
 	root.Right = left
 	return root
 }
+
+func invertTree2(root *TreeNode) *TreeNode {
+
+	if root == nil {
+		return root
+	}
+	stack := []*TreeNode{root}
+
+	for len(stack) > 0 {
+		pop := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		pop.Left, pop.Right = pop.Right, pop.Left
+
+		if pop.Left != nil {
+			stack = append(stack, pop.Left)
+		}
+
+		if pop.Right != nil {
+			stack = append(stack, pop.Right)
+		}
+	}
+	return root
+}
