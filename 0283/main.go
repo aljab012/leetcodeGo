2174fd9c657fd12ec6a96b5b3bbfe58e18d0bc96
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-07-15 12:05:37 +0400

    update #283

--- 0283/main.go
+++ 0283/main.go
@@ -2,16 +2,15 @@ package main
 
 func moveZeroes(nums []int) {
 	index := 0
-	for i := range nums {
-		if nums[i] == 0 {
-			continue
+
+	for _, n := range nums {
+		if n != 0 {
+			nums[index] = n
+			index++
 		}
-		nums[index] = nums[i]
-		index += 1
 	}
 	for index < len(nums) {
 		nums[index] = 0
-		index += 1
+		index++
 	}
-
 }
