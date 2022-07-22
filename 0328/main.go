804818d2606d9b3807a978cd667515c10efe2387
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-07-22 22:16:17 +0400

    Go #328

---
+++ 0328/main.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return head
+	}
+	oddHead, evenHead := head, head.Next
+	odd, even := head, head.Next
+
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return oddHead
+}
