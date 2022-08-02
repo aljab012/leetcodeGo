2c6bc6053bce4b9a39ac3dac0d7980a0cc657ed5
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-08-02 21:52:56 +0400

    Go #766

---
+++ 0766/main.go
@@ -0,0 +1,14 @@
+package main
+
+func isToeplitzMatrix(matrix [][]int) bool {
+	rows, cols := len(matrix), len(matrix[0])
+
+	for i := 1; i < rows; i++ {
+		for j := 1; j < cols; j++ {
+			if matrix[i][j] != matrix[i-1][j-1] {
+				return false
+			}
+		}
+	}
+	return true
+}
