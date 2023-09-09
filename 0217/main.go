d137c3750129785ade6b3c4c5fd75adb421376ce
Alhaitham Al Jabri <jabri.dev@gmail.com>
2023-09-09 23:09:12 +0400

    217: add test

--- 0217/main.go
+++ 0217/main.go
@@ -1,8 +1,12 @@
 package main
 
+// containsDuplicate checks if the given slice of integers contains duplicates.
+// It returns true if it does, false otherwise.
+// It uses a map to keep track of the numbers it has seen.
+// Time complexity: O(n)
+// Space complexity: O(n)
 func containsDuplicate(nums []int) bool {
 	nMap := make(map[int]bool)
-
 	for _, n := range nums {
 		_, found := nMap[n]
 		if found {
@@ -11,8 +15,4 @@ func containsDuplicate(nums []int) bool {
 		nMap[n] = true
 	}
 	return false
-}
-
-func main() {
-
 }
