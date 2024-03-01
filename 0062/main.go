565a06c930e70083d8d4047475fff5c00dcae40d
Alhaitham Al Jabri <jabri.dev@gmail.com>
2024-03-01 16:51:56 +0100

    62: add mulitple solutions

--- 0062/main.go
+++ 0062/main.go
@@ -1,8 +1,57 @@
 package main
 
-import "fmt"
+/*
+  - Intuition:
+  - 1. Draw tree diagram to understand the problem
+  - 2. The base case is when m and n are 1 (reached the end)
+  - 3. The base case is when m or n is 0 (out of bound)
+  - 4. Either move down or move right to reach the end of the grid which is m-1 or n-1
+*/
 
-func uniquePaths(m int, n int) int {
+/*
+ * Top-down recursive
+ */
+func uniquePaths1(m int, n int) int {
+	var fn func(int, int) int
+	fn = func(x int, y int) int {
+		if x == 1 && y == 1 { // base case reached the end
+			return 1
+		}
+		if x == 0 || y == 0 { // base case out of bound
+			return 0
+		}
+		return fn(x-1, y) + fn(x, y-1) // move down or move right
+	}
+	return fn(m, n)
+}
+
+/*
+ * Top-down recursive with memo
+ */
+func uniquePaths2(m int, n int) int {
+	type key struct {
+		x int
+		y int
+	}
+	memo := map[key]int{}
+	var fn func(int, int) int
+	fn = func(x int, y int) int {
+		if val, ok := memo[key{x, y}]; ok {
+			return val
+		}
+		if x == 1 && y == 1 {
+			return 1
+		}
+		if x == 0 || y == 0 {
+			return 0
+		}
+		ret := fn(x-1, y) + fn(x, y-1)
+		memo[key{x, y}] = ret
+		return ret
+	}
+	return fn(m, n)
+}
+func uniquePaths3(m int, n int) int {
 	if m == 0 || n == 0 {
 		return 0
 	}
@@ -16,9 +65,4 @@ func uniquePaths(m int, n int) int {
 		}
 	}
 	return arr[0]
-}
-
-func main() {
-	// expected: 3
-	fmt.Println(uniquePaths(3, 2))
 }
---
+++ 0062/main_test.go
@@ -0,0 +1,78 @@
+package main
+
+import "testing"
+
+func Test_uniquePaths1(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test 1", args{3, 7}, 28},
+		{"Test 2", args{3, 2}, 3},
+		{"Test 3", args{7, 3}, 28},
+		{"Test 4", args{3, 3}, 6},
+		{"Test 5", args{3, 4}, 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths1(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_uniquePaths2(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test 1", args{3, 7}, 28},
+		{"Test 2", args{3, 2}, 3},
+		{"Test 3", args{7, 3}, 28},
+		{"Test 4", args{3, 3}, 6},
+		{"Test 5", args{3, 4}, 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths2(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_uniquePaths3(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test 1", args{3, 7}, 28},
+		{"Test 2", args{3, 2}, 3},
+		{"Test 3", args{7, 3}, 28},
+		{"Test 4", args{3, 3}, 6},
+		{"Test 5", args{3, 4}, 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths3(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
