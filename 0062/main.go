96ac0b234957264dfedffc5ed35c5ef62207831e
Alhaitham Al Jabri <jabri.dev@gmail.com>
2024-03-03 22:56:36 +0100

    62: add tabulation solution

--- 0062/main.go
+++ 0062/main.go
@@ -14,23 +14,35 @@ package main
 
 /*
  * Top-down recursive
+ * Idea: Use recursion to move down or move right
+ * 	 The base case is when m and n are 1 (reached the end)
+ * 	 The base case is when m or n is 0 (out of bound)
+ * 	 The answer is the number of ways to reach the end of the grid which is m-1 or n-1
+ * 	 Time complexity: O(2^(m+n))
+ * 	 Space complexity: O(m+n)
  */
 func uniquePaths1(m int, n int) int {
 	var fn func(int, int) int
 	fn = func(x int, y int) int {
-		if x == 1 && y == 1 { // base case reached the end
+		if x == 1 && y == 1 { // base case: reached the end
 			return 1
 		}
-		if x == 0 || y == 0 { // base case out of bound
+		if x == 0 || y == 0 { // base case: out of bound
 			return 0
 		}
-		return fn(x-1, y) + fn(x, y-1) // move down or move right
+		return fn(x-1, y) + fn(x, y-1) // recursive call: move down or move right
 	}
 	return fn(m, n)
 }
 
 /*
  * Top-down recursive with memo
+ * Idea: Use recursion to move down or move right and store the result in a memoization table
+ * 	 The base case is when m and n are 1 (reached the end)
+ * 	 The base case is when m or n is 0 (out of bound)
+ * 	 The answer is the number of ways to reach the end of the grid which is m-1 or n-1
+ * 	 Time complexity: O(m*n)
+ * 	 Space complexity: O(m*n)
  */
 func uniquePaths2(m int, n int) int {
 	type key struct {
@@ -43,19 +55,52 @@ func uniquePaths2(m int, n int) int {
 		if val, ok := memo[key{x, y}]; ok {
 			return val
 		}
-		if x == 1 && y == 1 {
+		if x == 1 && y == 1 { // base case: reached the end
 			return 1
 		}
-		if x == 0 || y == 0 {
+		if x == 0 || y == 0 { // base case: out of bound
 			return 0
 		}
-		ret := fn(x-1, y) + fn(x, y-1)
+		ret := fn(x-1, y) + fn(x, y-1) // recursive call: move down or move right
 		memo[key{x, y}] = ret
 		return ret
 	}
 	return fn(m, n)
 }
+
+/*
+ * Top-down iterative with tabulation
+ * Idea: Use a 2D array to store the number of ways to reach each cell
+ * 	 The number of ways to reach a cell is the sum of the number of ways to reach the cell above and the cell to the left
+ * 	 The base case is the first row and the first column
+ * 	 The answer is the number of ways to reach the last cell
+ * 	 Time complexity: O(m*n)
+ * 	 Space complexity: O(m*n)
+ */
 func uniquePaths3(m int, n int) int {
+
+	matrix := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		matrix[i] = make([]int, n+1)
+	}
+
+	matrix[1][1] = 1 // base case
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			cur := matrix[i][j]
+			if i+1 <= m {
+				matrix[i+1][j] += cur // move down
+			}
+			if j+1 <= n {
+				matrix[i][j+1] += cur // move right
+			}
+		}
+	}
+	return matrix[m][n]
+
+}
+
+func uniquePaths4(m int, n int) int {
 	if m == 0 || n == 0 {
 		return 0
 	}
--- 0062/main_test.go
+++ 0062/main_test.go
@@ -76,3 +76,27 @@ func Test_uniquePaths3(t *testing.T) {
 		})
 	}
 }
+func Test_uniquePaths4(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test 1", args{3, 7}, 28},
+		{"Test 2", args{3, 2}, 3},
+		{"Test 3", args{7, 3}, 28},
+		{"Test 4", args{3, 3}, 6},
+		{"Test 5", args{3, 4}, 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths3(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
