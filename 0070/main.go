23691151f37d989c8ef6f778945faa4e73464c74
Alhaitham Al Jabri <jabri.dev@gmail.com>
2024-03-20 00:52:17 +0100

    70: drawings with solutions

--- 0070/main.go
+++ 0070/main.go
@@ -1,11 +1,66 @@
 package main
 
-// space complexity: O(1)
-// time complexity: O(n)
-func climbStairs(n int) int {
-	prevStep, curStep := 1, 1
-	for i := 2; i <= n; i++ {
-		curStep, prevStep = prevStep+curStep, curStep
+/*
+ * Top-down approach recursive
+ */
+func climbStairs1(n int) int {
+	var fn func(int) int
+	fn = func(num int) int {
+		if num == 0 {
+			return 1
+		}
+		if num < 0 {
+			return 0
+		}
+		return fn(num-1) + fn(num-2)
 	}
-	return curStep
+	return fn(n)
+}
+
+/*
+ * Top-down approach recursive with memoization
+ */
+func climbStairs2(n int) int {
+	memo := map[int]int{}
+	var fn func(int) int
+	fn = func(num int) int {
+		if val, ok := memo[num]; ok {
+			return val
+		}
+		if num == 0 {
+			return 1
+		}
+		if num < 0 {
+			return 0
+		}
+		ret := fn(num-1) + fn(num-2)
+		memo[num] = ret
+		return ret
+	}
+	return fn(n)
+}
+
+/*
+ * Bottom-up approach iterative with array
+ */
+func climbStairs3(n int) int {
+	arr := make([]int, n+1)
+	arr[0] = 1
+	arr[1] = 1
+	for i := 2; i <= n; i++ {
+		arr[i] = arr[i-1] + arr[i-2]
+	}
+	return arr[n]
+}
+
+/*
+/* Bottom-up approach iterative with variables
+*/
+func climbStairs4(n int) int {
+	first := 1
+	second := 1
+	for i := 2; i <= n; i++ {
+		second, first = first+second, second
+	}
+	return second
 }
--- 0070/main_test.go
+++ 0070/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func Test_climbStairs(t *testing.T) {
+func Test_climbStairs1(t *testing.T) {
 	type args struct {
 		n int
 	}
@@ -11,16 +11,108 @@ func Test_climbStairs(t *testing.T) {
 		args args
 		want int
 	}{
-		{"Test 0 stairs", args{1}, 1},
-		{"Test 1 stairs", args{2}, 2},
-		{"Test 2 stairs ", args{3}, 3},
-		{"Test 3 stairs", args{4}, 5},
-		{"Test 4 stairs", args{5}, 8},
+		{"Test 1", args{2}, 2},
+		{"Test 2", args{3}, 3},
+		{"Test 3", args{4}, 5},
+		{"Test 4", args{5}, 8},
+		{"Test 5", args{6}, 13},
+		{"Test 6", args{7}, 21},
+		{"Test 7", args{8}, 34},
+		{"Test 8", args{9}, 55},
+		{"Test 9", args{10}, 89},
+		{"Test 10", args{11}, 144},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := climbStairs(tt.args.n); got != tt.want {
-				t.Errorf("climbStairs() = %v, want %v", got, tt.want)
+			if got := climbStairs1(tt.args.n); got != tt.want {
+				t.Errorf("climbStairs1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_climbStairs2(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test 1", args{2}, 2},
+		{"Test 2", args{3}, 3},
+		{"Test 3", args{4}, 5},
+		{"Test 4", args{5}, 8},
+		{"Test 5", args{6}, 13},
+		{"Test 6", args{7}, 21},
+		{"Test 7", args{8}, 34},
+		{"Test 8", args{9}, 55},
+		{"Test 9", args{10}, 89},
+		{"Test 10", args{11}, 144},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := climbStairs2(tt.args.n); got != tt.want {
+				t.Errorf("climbStairs2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_climbStairs3(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test 1", args{2}, 2},
+		{"Test 2", args{3}, 3},
+		{"Test 3", args{4}, 5},
+		{"Test 4", args{5}, 8},
+		{"Test 5", args{6}, 13},
+		{"Test 6", args{7}, 21},
+		{"Test 7", args{8}, 34},
+		{"Test 8", args{9}, 55},
+		{"Test 9", args{10}, 89},
+		{"Test 10", args{11}, 144},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := climbStairs3(tt.args.n); got != tt.want {
+				t.Errorf("climbStairs3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_climbStairs4(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test 1", args{2}, 2},
+		{"Test 2", args{3}, 3},
+		{"Test 3", args{4}, 5},
+		{"Test 4", args{5}, 8},
+		{"Test 5", args{6}, 13},
+		{"Test 6", args{7}, 21},
+		{"Test 7", args{8}, 34},
+		{"Test 8", args{9}, 55},
+		{"Test 9", args{10}, 89},
+		{"Test 10", args{11}, 144},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := climbStairs4(tt.args.n); got != tt.want {
+				t.Errorf("climbStairs4() = %v, want %v", got, tt.want)
 			}
 		})
 	}
