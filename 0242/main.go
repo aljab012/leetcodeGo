163f5cb64a311cbdf653c8ef6018be0e0702f4ab
Alhaitham Al Jabri <jabri.dev@gmail.com>
2024-04-06 13:09:05 +0200

    0242: add second solution

--- 0242/main.go
+++ 0242/main.go
@@ -1,9 +1,16 @@
 package main
 
-// isAnagram checks if two strings are anagrams
-// Time Complexity: O(n)
-// Space Complexity: O(1)
-func isAnagram(s string, t string) bool {
+import (
+	"sort"
+	"strings"
+)
+
+/*
+ * solution using a map to store the number and check if the number is already in the map
+ * Space complexity: O(n)
+ * Time complexity: O(n)
+ */
+func isAnagram1(s string, t string) bool {
 
 	letters := make([]int, 26)
 
@@ -21,4 +28,20 @@ func isAnagram(s string, t string) bool {
 	}
 	return true
 
+}
+
+/*
+ * solution using sorting
+ * Space complexity: O(n)
+ * Time complexity: O(nlogn)
+ */
+func isAnagram2(s string, t string) bool {
+	s = sortString(s)
+	t = sortString(t)
+	return s == t
+}
+func sortString(s string) string {
+	split := strings.Split(s, "")
+	sort.Strings(split)
+	return strings.Join(split, "")
 }
--- 0242/main_test.go
+++ 0242/main_test.go
@@ -1,8 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_isAnagram(t *testing.T) {
+func Test_isAnagram1(t *testing.T) {
 	type args struct {
 		s string
 		t string
@@ -12,21 +14,61 @@ func Test_isAnagram(t *testing.T) {
 		args args
 		want bool
 	}{
-		{
-			name: "Test Anagram",
-			args: args{s: "anagram", t: "nagaram"},
-			want: true,
-		},
-		{
-			name: "Not Anagram",
-			args: args{s: "rat", t: "car"},
-			want: false,
-		},
+		{"case1", args{"anagram", "nagaram"}, true,},
+		{"case2", args{"rat", "car"}, false,},
+		{"case3", args{"a", "ab"}, false,},
+		{"case4", args{"a", "a"}, true,},
+		{"case5", args{"a", "b"}, false,},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := isAnagram(tt.args.s, tt.args.t); got != tt.want {
-				t.Errorf("isAnagram() = %v, want %v", got, tt.want)
+			if got := isAnagram1(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("isAnagram1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isAnagram2(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"case1", args{"anagram", "nagaram"}, true,},
+		{"case2", args{"rat", "car"}, false,},
+		{"case3", args{"a", "ab"}, false,},
+		{"case4", args{"a", "a"}, true,},
+		{"case5", args{"a", "b"}, false,},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAnagram2(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("isAnagram2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sortString(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortString(tt.args.s); got != tt.want {
+				t.Errorf("sortString() = %v, want %v", got, tt.want)
 			}
 		})
 	}
