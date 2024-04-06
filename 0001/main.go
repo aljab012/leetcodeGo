052b8717720e13c9f1388ff56e083d6bfdc8cb31
Alhaitham Al Jabri <jabri.dev@gmail.com>
2024-04-06 13:18:49 +0200

    0001: add all solutions

--- 0001/main.go
+++ 0001/main.go
@@ -1,17 +1,54 @@
 package main
 
-// time: O(n)
-// space: O (n)
-// Idea: interate through the array, see if you have encountered a complement in the previous nums.
-func twoSum(nums []int, target int) []int {
-	numMap := make(map[int]int)
+/*
+ * solution using two loops
+ * Space complexity: O(1)
+ * Time complexity: O(n^2)
+ */
+func twoSum1(nums []int, target int) []int {
+	for i, n := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if n+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{-1, -1}
+}
+
+/*
+ * solution using a map to store all the numbers and check if the complement is in the map
+ * Space complexity: O(n)
+ * Time complexity: O(n)
+ */
+func twoSum2(nums []int, target int) []int {
+	nMap := map[int]int{}
+	for i, n := range nums {
+		nMap[n] = i
+	}
 	for i, n := range nums {
 		comp := target - n
-		number, found := numMap[comp]
-		if found {
-			return []int{number, i}
+		if val, ok := nMap[comp]; ok && val != i {
+			return []int{i, val}
 		}
-		numMap[n] = i
+	}
+	return []int{-1, -1}
+}
+
+/*
+ * using a map to store the number and check if the number is already in the map
+ * Space complexity: O(n)
+ * Time complexity: O(n)
+ */
+// Idea: interate through the array, see if you have encountered a complement in the previous nums.
+func twoSum3(nums []int, target int) []int {
+	nMap := map[int]int{}
+	for i, n := range nums {
+		comp := target - n
+		if val, ok := nMap[comp]; ok {
+			return []int{val, i}
+		}
+		nMap[n] = i
 	}
 	return []int{-1, -1}
 }
--- 0001/main_test.go
+++ 0001/main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_twoSum(t *testing.T) {
+func Test_twoSum1(t *testing.T) {
 	type args struct {
 		nums   []int
 		target int
@@ -15,19 +15,63 @@ func Test_twoSum(t *testing.T) {
 		args args
 		want []int
 	}{
-		{
-			name: "[2,7,11,15], 9",
-			args: args{
-				nums:   []int{2, 7, 11, 15},
-				target: 9,
-			},
-			want: []int{0, 1},
-		},
+		{"test case 1", args{[]int{2, 7, 11, 15}, 9}, []int{0, 1}},
+		{"test case 2", args{[]int{3, 2, 4}, 6}, []int{1, 2}},
+		{"test case 3", args{[]int{3, 3}, 6}, []int{0, 1}},
+		{"test case 4", args{[]int{3, 2, 3}, 6}, []int{0, 2}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := twoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			if got := twoSum1(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_twoSum2(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"test case 1", args{[]int{2, 7, 11, 15}, 9}, []int{0, 1}},
+		{"test case 2", args{[]int{3, 2, 4}, 6}, []int{1, 2}},
+		{"test case 3", args{[]int{3, 3}, 6}, []int{0, 1}},
+		{"test case 4", args{[]int{3, 2, 3}, 6}, []int{0, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum2(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_twoSum3(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"test case 1", args{[]int{2, 7, 11, 15}, 9}, []int{0, 1}},
+		{"test case 2", args{[]int{3, 2, 4}, 6}, []int{1, 2}},
+		{"test case 3", args{[]int{3, 3}, 6}, []int{0, 1}},
+		{"test case 4", args{[]int{3, 2, 3}, 6}, []int{0, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum3(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum3() = %v, want %v", got, tt.want)
 			}
 		})
 	}
