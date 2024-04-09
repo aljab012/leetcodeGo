3e26f91d8c454d1721d9c4c0205a2b6b1ea58f5d
Alhaitham Al Jabri <jabri.dev@gmail.com>
2024-04-09 15:41:01 +0200

    0125: comment with tests

--- 0125/main.go
+++ 0125/main.go
@@ -2,21 +2,26 @@ package main
 
 import "unicode"
 
+/*
+ * Two pointers approach
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ */
 func isPalindrome(s string) bool {
 	chars := []rune(s)
 
-	p1 := 0
-	p2 := len(chars) - 1
+	l := 0
+	r := len(chars) - 1
 
-	for p1 < p2 {
+	for l < r {
 
-		if !unicode.IsNumber(chars[p1]) && !unicode.IsLetter(chars[p1]) {
-			p1++
-		} else if !unicode.IsNumber(chars[p2]) && !unicode.IsLetter(chars[p2]) {
-			p2--
-		} else if unicode.ToLower(chars[p1]) == unicode.ToLower(chars[p2]) {
-			p1++
-			p2--
+		if !unicode.IsNumber(chars[l]) && !unicode.IsLetter(chars[l]) {
+			l++
+		} else if !unicode.IsNumber(chars[r]) && !unicode.IsLetter(chars[r]) {
+			r--
+		} else if unicode.ToLower(chars[l]) == unicode.ToLower(chars[r]) {
+			l++
+			r--
 		} else {
 			return false
 		}
---
+++ 0125/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Example 1",
+			args: args{s: "racecar"},
+			want: true,
+		},
+		{
+			name: "Example 2",
+			args: args{s: "hello"},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
