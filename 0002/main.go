3c7c9e7afff0444e1c4edc391b7a3b7064fb64f4
Alhaitham Al Jabri <jabri.dev@gmail.com>
2024-04-13 20:54:30 +0200

    0002: add tests

--- 0002/main.go
+++ 0002/main.go
@@ -5,6 +5,38 @@ type ListNode struct {
 	Next *ListNode
 }
 
+/*
+ * 2. Add Two Numbers
+ * Intuition: We can add two numbers by iterating through both lists and adding the values of the nodes along with the carry.
+ * Time complexity: O(max(m, n)) where m and n are the lengths of the two input lists.
+ * Space complexity: O(max(m, n)) where m and n are the lengths of the two input lists.
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	cur := dummy
+	carry := 0
+	for l1 != nil || l2 != nil {
+		v1, v2 := 0, 0
+		if l1 != nil {
+			v1 += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			v2 += l2.Val
+			l2 = l2.Next
+		}
+		sum := (v1 + v2 + carry) % 10
+		carry = (v1 + v2 + carry) / 10
+		cur.Next = &ListNode{Val: sum}
+		cur = cur.Next
+	}
+	if carry != 0 {
+		cur.Next = &ListNode{Val: 1}
+	}
+	return dummy.Next
+}
+
+/*
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	dummy := new(ListNode)
@@ -27,3 +59,4 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	return dummy.Next
 
 }
+*/
