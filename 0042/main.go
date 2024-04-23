974907bed4ccd362259ac0eb16febf1e977ee711
Alhaitham Al Jabri <jabri.dev@gmail.com>
2024-04-23 19:08:15 +0200

    0042: more solutions

--- 0042/main.go
+++ 0042/main.go
@@ -1,13 +1,75 @@
 package main
 
-func trap(height []int) int {
-	// water always bound by the min of left and right
+/*
+ * Brute force solution
+ * For each element, find the left and right minimum height and calculate the water
+ * Time complexity: O(n^2)
+ * Space complexity: O(1)
+ */
+func trap1(height []int) int {
+	ret := 0
+	for i := 0; i < len(height); i++ {
+		lMax, rMax := 0, 0
+		// find the left max
+		for j := i; j >= 0; j-- {
+			lMax = max(lMax, height[j])
+		}
+		// find the right max
+		for j := i; j < len(height); j++ {
+			rMax = max(rMax, height[j])
+		}
+		// calculate the water bounded by the min of left and right
+		ret += min(lMax, rMax) - height[i]
+	}
+	return ret
+}
+
+/*
+ * Optimize solution using space to store the left and right max height for each element
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+func trap2(height []int) int {
+	ret := 0
+	lMax := make([]int, len(height))
+	rMax := make([]int, len(height))
+
+	// find the left max for each element
+	lMax[0] = height[0]
+	for i := 1; i < len(height); i++ {
+		lMax[i] = max(lMax[i-1], height[i])
+	}
+	// find the right max for each element
+	rMax[len(height)-1] = height[len(height)-1]
+	for i := len(height) - 2; i >= 0; i-- {
+		rMax[i] = max(rMax[i+1], height[i])
+	}
+
+	// calculate the water for each element which is bounded by the min of left and right
+	for i := 0; i < len(height); i++ {
+		ret += min(lMax[i], rMax[i]) - height[i]
+	}
+	return ret
+}
+
+/*
+ * Optimize solution using two pointers
+ * The water is always bounded by the min of left and right
+ * The intuition is to use two pointers to track the left and right max height
+ * If the left height is less than the right height, the water is bounded by the left height
+ * Otherwise, the water is bounded by the right height
+ * The water is the min of left and right height minus the current height
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ */
+func trap3(height []int) int {
 	ret := 0
 	l, r := 0, len(height)-1
 	lMax, rMax := 0, 0
 	for l < r {
 		lMax = max(lMax, height[l])
 		rMax = max(rMax, height[r])
+		// if the left height is less than the right height, the water is bounded by the left height
 		if height[l] < height[r] {
 			water := min(lMax, rMax) - height[l]
 			if water > 0 {
@@ -15,6 +77,7 @@ func trap(height []int) int {
 			}
 			l++
 		} else {
+			// otherwise, the water is bounded by the right height
 			water := min(lMax, rMax) - height[r]
 			if water > 0 {
 				ret += water
@@ -31,6 +94,7 @@ func min(x, y int) int {
 	}
 	return y
 }
+
 func max(x, y int) int {
 	if x > y {
 		return x
--- 0042/main_test.go
+++ 0042/main_test.go
@@ -1,32 +1,8 @@
 package main
 
-import "testing"
-
-func Test_trap(t *testing.T) {
-	type args struct {
-		height []int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{"Example 1", args{height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
-		{"Example 2", args{height: []int{4, 2, 0, 3, 2, 5}}, 9},
-		{"Example 3", args{height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
-		{"Example 4", args{height: []int{4, 2, 0, 3, 2, 5}}, 9},
-		{"Example 5", args{height: []int{4, 2, 3}}, 1},
-		{"Example 6", args{height: []int{4, 2, 3, 1}}, 1},
-		{"Example 7", args{height: []int{4, 2, 3, 1, 2}}, 2},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := trap(tt.args.height); got != tt.want {
-				t.Errorf("trap() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+import (
+	"testing"
+)
 
 func Test_min(t *testing.T) {
 	type args struct {
@@ -69,6 +45,84 @@ func Test_max(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := max(tt.args.x, tt.args.y); got != tt.want {
 				t.Errorf("max() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_trap1(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
+		{"Example 2", args{height: []int{4, 2, 0, 3, 2, 5}}, 9},
+		{"Example 3", args{height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
+		{"Example 4", args{height: []int{4, 2, 0, 3, 2, 5}}, 9},
+		{"Example 5", args{height: []int{4, 2, 3}}, 1},
+		{"Example 6", args{height: []int{4, 2, 3, 1}}, 1},
+		{"Example 7", args{height: []int{4, 2, 3, 1, 2}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trap1(tt.args.height); got != tt.want {
+				t.Errorf("trap1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_trap2(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
+		{"Example 2", args{height: []int{4, 2, 0, 3, 2, 5}}, 9},
+		{"Example 3", args{height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
+		{"Example 4", args{height: []int{4, 2, 0, 3, 2, 5}}, 9},
+		{"Example 5", args{height: []int{4, 2, 3}}, 1},
+		{"Example 6", args{height: []int{4, 2, 3, 1}}, 1},
+		{"Example 7", args{height: []int{4, 2, 3, 1, 2}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trap2(tt.args.height); got != tt.want {
+				t.Errorf("trap2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_trap3(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
+		{"Example 2", args{height: []int{4, 2, 0, 3, 2, 5}}, 9},
+		{"Example 3", args{height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
+		{"Example 4", args{height: []int{4, 2, 0, 3, 2, 5}}, 9},
+		{"Example 5", args{height: []int{4, 2, 3}}, 1},
+		{"Example 6", args{height: []int{4, 2, 3, 1}}, 1},
+		{"Example 7", args{height: []int{4, 2, 3, 1, 2}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trap3(tt.args.height); got != tt.want {
+				t.Errorf("trap3() = %v, want %v", got, tt.want)
 			}
 		})
 	}
