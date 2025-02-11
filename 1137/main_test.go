1db78756905ed0caf8bb58e583f8053ac64cdc91
Alhaitham Al Jabri <jabri.dev@gmail.com>
2025-02-11 23:11:06 +0400

    1137: shorten tests

--- 1137/main_test.go
+++ 1137/main_test.go
@@ -1,86 +1,38 @@
 package main
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-func Test_tribonacci1(t *testing.T) {
-	type args struct {
-		n int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
+func TestTribonacci(t *testing.T) {
+	testCases := []struct {
+		n, want int
 	}{
-		{"Test 1", args{4}, 4},
-		{"Test 2", args{25}, 1389537},
+		{4, 4},
+		{25, 1389537},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tribonacci1(tt.args.n); got != tt.want {
-				t.Errorf("tribonacci1() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func Test_tribonacci2(t *testing.T) {
-	type args struct {
-		n int
-	}
-	tests := []struct {
+	funcs := []struct {
 		name string
-		args args
-		want int
+		fn   func(int) int
 	}{
-		{"Test 1", args{4}, 4},
-		{"Test 2", args{25}, 1389537},
+		{"tribonacci1", tribonacci1},
+		{"tribonacci2", tribonacci2},
+		{"tribonacci3", tribonacci3},
+		{"tribonacci4", tribonacci4},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tribonacci2(tt.args.n); got != tt.want {
-				t.Errorf("tribonacci2() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func Test_tribonacci3(t *testing.T) {
-	type args struct {
-		n int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{"Test 1", args{4}, 4},
-		{"Test 2", args{25}, 1389537},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tribonacci3(tt.args.n); got != tt.want {
-				t.Errorf("tribonacci3() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_tribonacci4(t *testing.T) {
-	type args struct {
-		n int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{"Test 1", args{4}, 4},
-		{"Test 2", args{25}, 1389537},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tribonacci4(tt.args.n); got != tt.want {
-				t.Errorf("tribonacci4() = %v, want %v", got, tt.want)
+	for _, f := range funcs {
+		f := f
+		t.Run(f.name, func(t *testing.T) {
+			for _, tc := range testCases {
+				tc := tc
+				t.Run("n="+strconv.Itoa(tc.n), func(t *testing.T) {
+					if got := f.fn(tc.n); got != tc.want {
+						t.Errorf("for n=%d: got %d, want %d", tc.n, got, tc.want)
+					}
+				})
 			}
 		})
 	}
