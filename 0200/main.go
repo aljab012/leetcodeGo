5b38b07bd250c3d95f9f887890ddb91d58a6bd7e
Alhaitham Al Jabri <jabri.dev@gmail.com>
2025-02-12 22:41:01 +0400

    0200: solution with tests

--- 0200/main.go
+++ 0200/main.go
@@ -1,29 +1,26 @@
 package main
 
 func numIslands(grid [][]byte) int {
-	if len(grid) == 0 || len(grid[0]) == 0 {
-		return 0
-	}
-
-	ret := 0
+	count := 0
 	for i := 0; i < len(grid); i++ {
-		for j := 0; j < len(grid[0]); j++ {
+		for j := 0; j < len(grid[i]); j++ {
 			if grid[i][j] == '1' {
-				grid = DFS(i, j, grid)
-				ret++
+				dfs(grid, i, j)
+				count++
 			}
 		}
 	}
-	return ret
+	return count
 }
-func DFS(i, j int, grid [][]byte) [][]byte {
-	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[i]) || grid[i][j] != '1' {
-		return grid
+func dfs(grid [][]byte, i, j int) {
+	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[i]) || grid[i][j] != '1' {
+		return
 	}
+
 	grid[i][j] = '0'
-	grid = DFS(i+1, j, grid)
-	grid = DFS(i-1, j, grid)
-	grid = DFS(i, j+1, grid)
-	grid = DFS(i, j-1, grid)
-	return grid
+
+	dfs(grid, i+1, j)
+	dfs(grid, i-1, j)
+	dfs(grid, i, j+1)
+	dfs(grid, i, j-1)
 }
