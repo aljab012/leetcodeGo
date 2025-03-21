eadc2ed548ee17e1d2c9a7f4f13e4402c2229b9d
Alhaitham Al Jabri <jabri.dev@gmail.com>
2025-03-21 23:37:26 +0400

    0112: solution

--- 0112/main.go
+++ 0112/main.go
@@ -7,18 +7,11 @@ type TreeNode struct {
 }
 
 func hasPathSum(root *TreeNode, targetSum int) bool {
-
-	return helper(root, targetSum)
-}
-
-func helper(root *TreeNode, targetSum int) bool {
-
 	if root == nil {
 		return false
 	}
-	sum := targetSum - root.Val
-	if sum == 0 && root.Left == nil && root.Right == nil {
+	if root.Left == nil && root.Right == nil && targetSum-root.Val == 0 {
 		return true
 	}
-	return helper(root.Left, sum) || helper(root.Right, sum)
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
 }
