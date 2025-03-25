265b16e89762aa05d8b9ebc1e484a4c3396682d1
Alhaitham Al Jabri <jabri.dev@gmail.com>
2025-03-25 20:51:29 +0400

    2331: solution with test

---
+++ 2331/main.go
@@ -0,0 +1,23 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func evaluateTree(root *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	if root.Val == 0 {
+		return false
+	}
+	if root.Val == 1 {
+		return true
+	}
+	if root.Val == 2 {
+		return evaluateTree(root.Left) || evaluateTree(root.Right)
+	}
+	return evaluateTree(root.Left) && evaluateTree(root.Right)
+}
