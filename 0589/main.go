6375bd11b5e83c163c9f5876ca987605acbf8497
Alhaitham Al Jabri <jabri.dev@gmail.com>
2025-04-03 10:48:24 +0400

    0589: solution

--- 0589/main.go
+++ 0589/main.go
@@ -5,21 +5,46 @@ type Node struct {
 	Children []*Node
 }
 
-func preorder(root *Node) []int {
-	return helper(root, []int{})
-}
+/*
+ * Recursive
+ */
+func preorder1(root *Node) []int {
+	ret := []int{}
 
-func helper(root *Node, ret []int) []int {
-	if root == nil {
-		return ret
+	var visit func(node *Node)
+	visit = func(node *Node) {
+		if node == nil {
+			return
+		}
+		ret = append(ret, node.Val)
+		for _, child := range node.Children {
+			visit(child)
+		}
 	}
-	ret = append(ret, root.Val)
-	for i := range root.Children {
-		ret = helper(root.Children[i], ret)
-	}
+
+	visit(root)
+
 	return ret
 }
 
-func main() {
+/*
+ * Iterative
+ */
 
+func preorder2(root *Node) []int {
+	if root == nil {
+		return []int{}
+	}
+	ret := []int{}
+	stack := []*Node{root}
+	for len(stack) > 0 {
+		pop := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		ret = append(ret, pop.Val)
+		for i := len(pop.Children) - 1; i >= 0; i-- {
+			stack = append(stack, pop.Children[i])
+		}
+	}
+	return ret
 }
