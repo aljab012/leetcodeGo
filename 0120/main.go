de7bf09eec139a9ed029e67d9fa1b133e5f83a00
Alhaitham Al Jabri <jabri.dev@gmail.com>
2025-04-06 02:37:03 +0400

    0120: solution with test

---
+++ 0120/main.go
@@ -0,0 +1,27 @@
+package main
+
+func minimumTotal(triangle [][]int) int {
+	rows := len(triangle)
+	memo := map[[2]int]int{}
+
+	var dfs func(r, c int) int
+	dfs = func(r, c int) int {
+		if val, ok := memo[[2]int{r, c}]; ok {
+			return val
+		}
+		if r == rows-1 {
+			return triangle[r][c]
+		}
+		ret := triangle[r][c] + min(dfs(r+1, c), dfs(r+1, c+1))
+		memo[[2]int{r, c}] = ret
+		return ret
+	}
+	return dfs(0, 0)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
