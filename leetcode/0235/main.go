2c7418a4fd654c22e8d48d24a5a891d9a1721a7d
Alhaitham Al Jabri <jabri.dev@gmail.com>
2025-05-23 03:35:06 +0400

    0235: solution

--- leetcode/0235/main.go
+++ leetcode/0235/main.go
@@ -6,12 +6,33 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+/*
+ * Recursive solution
+ */
+func lowestCommonAncestor1(root, p, q *TreeNode) *TreeNode {
 	if p.Val < root.Val && q.Val < root.Val {
-		return lowestCommonAncestor(root.Left, p, q)
+		return lowestCommonAncestor1(root.Left, p, q)
 	} else if p.Val > root.Val && q.Val > root.Val {
-		return lowestCommonAncestor(root.Right, p, q)
+		return lowestCommonAncestor1(root.Right, p, q)
 	} else {
 		return root
 	}
+}
+
+/*
+ * Iterative solution
+ *
+ */
+func lowestCommonAncestor2(root, p, q *TreeNode) *TreeNode {
+	cur := root
+	for cur != nil {
+		if p.Val < cur.Val && q.Val < cur.Val {
+			cur = cur.Left
+		} else if p.Val > cur.Val && q.Val > cur.Val {
+			cur = cur.Right
+		} else {
+			return cur
+		}
+	}
+	return nil
 }
